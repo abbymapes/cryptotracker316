52decb61c824b62053573a68d81253a588eff26a
Aparimeya Taneja <at307@duke.edu>
2020-10-05 20:20:22 +0530

    simple firestore read test

--- functions/src/routes/index.svelte
+++ functions/src/routes/index.svelte
@@ -1,46 +1,27 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<script>
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+	import firebase from 'firebase/app'
 
-	figure {
-		margin: 0 0 1em 0;
-	}
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+	import { onMount } from "svelte";
+	onMount(()=>{
+		const db =  firebase.firestore()
+		db.collection("test").get().then(snap =>{
+			snap.forEach(item=>{
+				 console.log(item.id, " => ", item.data());
+			})
+		})
+	})
 
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
+</script>
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Crypto Tracker</h1>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+
+<p>
+	<b> Basic Starter Template.</b>
+</p>
