fd956cc7df82f198f6c67ca2f9ed2a8bd44530ba
Aparimeya Taneja <at307@duke.edu>
2020-10-07 21:28:30 +0530

    purge

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-const functions = require('firebase-functions');
-const {expressServer} = require('./__sapper__/build/server/server.js')
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.ssr = functions.https.onRequest(expressServer)
