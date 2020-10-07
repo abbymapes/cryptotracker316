c35e8b36e6592ba4feaa1df951a23a958a4d66c4
Abby Mapes <acm103@duke.edu>
2020-10-07 18:09:56 -0400

    Add pages for logged in users

---
+++ frontend/src/routes/authUser/feed.svelte
@@ -0,0 +1,28 @@
+<script>
+  import TransactionPost from '../../components/TransactionPost.svelte';
+  import firebase from 'firebase/app';
+  import {onMount} from 'svelte';
+
+    onMount(()=>{
+      const db =  firebase.firestore()
+      db.collection("test").get().then(snap =>{
+        snap.forEach(item=>{
+           console.log(item.id, " => ", item.data());
+        })
+      })
+    })
+</script>
+
+<style>
+</style>
+
+<head>
+  <title>Feed</title>
+</head>
+
+<TransactionPost/>
+<br>
+<TransactionPost/>
+<br>
+<TransactionPost/>
+<br>
