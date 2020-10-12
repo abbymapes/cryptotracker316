08ad54eb5fbcbb476ea12dd26c4037d16f2cfaa2
Aparimeya Taneja <at307@duke.edu>
2020-10-13 02:46:11 +0530

    c

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,20 +1,49 @@
-
-
-
 <script>
+	import firebase from 'firebase/app';
+	import {onMount} from 'svelte';
+  import { goto,stores } from '@sapper/app';
+  export let segment;
 
-	import { stores } from '@sapper/app';
-	import { onMount } from "svelte";
+  const {session} = stores()
+  let currencies = ["BTC","ETH","LTC"]
+  let requests = []
+  let x = []
+	onMount( async ()=>{
+    for(let i = 0 ; i < 3; i++)
+      requests = [...requests, fetch(`https://api.coinbase.com/v2/prices/${currencies[i]}-USD/spot`)] 
+      let g =  await getData()
+      g.forEach(async r=>{
+        let re = await r.json()
+        console.log(re.data)
+        x = [...x,re.data]
+      })
+      
 
-	const {session} = stores()
-	onMount( async()=>{
-		 fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
-		.then(res=>res.json())
-		.then(data => console.log(data));
-		
-	})
+  })
+
+  async function getData(){
+    const res = await Promise.all(requests)
+    return res
+
+  }
 
 </script>
-{#if $session.user}
-	<h1>Logged In</h1>
-{/if}
+<div class="bar">
+  {#each currencies as y,i}
+    <span>
+      {y} {#if x[i]} ${x[i].amount} {/if}
+    </span>
+  {/each}
+</div>
+<style>
+.bar{
+  background-color: #2A2A2A;
+  color: white;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding:5px;
+  justify-content: space-around;
+}
+
+</style>
