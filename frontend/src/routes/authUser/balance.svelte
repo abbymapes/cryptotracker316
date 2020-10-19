de04d7cba01031436667ce3dd46c859b7313b888
Apoorv Jha <apoorvjha@MacBook-Pro.local>
2020-10-19 15:50:19 +0530

    ms2 edits

--- frontend/src/routes/authUser/balance.svelte
+++ frontend/src/routes/authUser/balance.svelte
@@ -2,34 +2,25 @@
   let balance = 1000;
   let numbers = 500;
   let yes = false;
-  
   let questions = [
 		{ id: 1, text: `Add Balance` },
 		{ id: 2, text: `Withdraw balance` },
 	];
-
 	let selected;
-
-
-
   function addNumber() {
     balance += numbers;
     alert('Balance Added!');
 	}
-
 </script>
 
 <style>
-
   p {
 		color: black;
 		font-family: 'Arial';
 		font-size: 2em;
     align-items: center;
 	}
-
   input { display: block; width: 2672px; max-width: 100%;}
-
   button {
 		height: 4rem;
 		width: 32rem;
@@ -45,30 +36,25 @@
 		background-position: 0;
 		color:black;
 	}
-  
 </style>
-
 
 <h1> <title>Balance</title> </h1>
 <p> BALANCE = ${balance} </p>
 
+<!-- svelte-ignore a11y-no-onchange -->
 <select bind:value={selected} on:change="{() => numbers = ''}">
 {#each questions as question}
 <option value={question}>
-      {question.text}
-    </option>
-  {/each}
+  {question.text}
+  </option>
+{/each}
 </select>
 
-  <input type=number bind:value={numbers} min=0 max=1000>
-  <input type=range bind:value={numbers} min=0 max=1000>
+<input type=number bind:value={numbers} min=0 max=1000>
+<input type=range bind:value={numbers} min=0 max=1000>
 
-    
-  <input type=checkbox bind:checked={yes}> Check box to continue adding balance.
-    {#if yes}
-  <h3>Thank you. Click button to continue.</h3>
-    {:else}
-  <h3>You must check box to continue.</h3>
-    {/if}
-  <button disabled={!yes} on:click|preventDefault={addNumber }>Add ${numbers} to balance</button>
+<input type=checkbox bind:checked={yes}> <h6>Check box to continue adding balance.</h6>
+{#if yes} <h3>Thank you. Click button to continue.</h3>
+{:else} <h3>You must check box to continue.</h3>
+{/if} <button disabled={!yes} on:click|preventDefault={addNumber }>Add ${numbers} to balance</button>
 
