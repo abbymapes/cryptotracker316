1112fbf1be060b7cc75d69b47127e1256586e7a4
Julius <jy217@duke.edu>
2020-11-06 14:39:17 -0500

    start of transaction

---
+++ frontend/src/routes/transaction.svelte
@@ -0,0 +1,46 @@
+
+
+
+
+<script>
+
+import { goto,stores } from '@sapper/app';
+import { onMount } from "svelte";
+
+
+const currencies = ["BTC","ETH","LTC"]
+const { session } = stores();
+
+let currentUid
+onMount(()=>{currentUid = firebase.auth().currentUser ? firebase.auth().currentUser.uid : undefined})
+
+async function trans() {
+    // Add a new document in collection "cities"
+    currentUid.
+    firebase.firestore().collection("transaction").doc().set({
+        amount: 1,
+        caption: "hello",
+        comment_count: 0,
+        like_count: 0,
+        stock: "BTC",
+        time: firebase.firestore.FieldValue.serverTimestamp(),
+        type: "buy",
+        uid: currentUid
+
+    })
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+});
+
+}
+
+</script>
+
+
+
+<button on:click={trans}>
+    Add Comment
+</button>
