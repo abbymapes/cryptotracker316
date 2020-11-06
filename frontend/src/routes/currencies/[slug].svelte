5d2fa20c9f8ff35d6dade25a550afbfe5ec5ffa2
Isabel Friedman <isf@duke.edu>
2020-11-06 11:39:58 -0500

    try1

--- frontend/src/routes/currencies/[slug].svelte
+++ frontend/src/routes/currencies/[slug].svelte
@@ -1,10 +1,74 @@
 <script>
     import { stores } from "@sapper/app";
     const { page,session} = stores()
-
+    import { onMount } from "svelte";
     const { slug } = $page.params;
+
+    /*
+    for given symbol (slug) fetch the price from last x hours (enough to populate chart)
+    and then give it to pre built charting library (pancake etc) -- like package in python
+    don't worry about style
+    */
+   //show current price  
+   const map = {
+       'BTC':'Bitcoin',
+       'XRP':'Ripple',
+       'LTC':'Litecoin',
+       'DOGE':'Dogecoin',
+       'ETH':'Ethereum',
+       'LINK':'Chainlink',
+       'BCH':'Bitcoin Cash',
+       'BNB':'Binance Coin'       
+   }
+   onMount(()=>{
+    getTrades()
+//     new TradingView.widget(
+//     {
+//     "width": 980,
+//     "height": 610,
+//     "symbol": "NASDAQ:AAPL",
+//     "interval": "D",
+//     "timezone": "Etc/UTC",
+//     "theme": "light",
+//     "style": "1",
+//     "locale": "in",
+//     "toolbar_bg": "#f1f3f6",
+//     "enable_publishing": false,
+//     "allow_symbol_change": true,
+//     "container_id": "tradingview_bc973"
+//   }
+//     );
+   })
+let trades = []
+   async function getTrades() {
+    await firebase.firestore().collection("transaction").where("stock", "==", slug.toUpperCase()).limit(5)
+        .get()
+        .then(snap=>{
+            snap.forEach(doc=>{
+                trades = [...trades,doc.data()]
+                console.log(doc.data())
+            })
+        })
+  }
+
 </script>
 
+<!-- TradingView Widget BEGIN -->
+<!-- <div class="tradingview-widget-container">
+    <div id="tradingview_bc973"></div>
+    <div class="tradingview-widget-copyright"><a href="https://in.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+   
+  
 
 
-{slug}
+  </div> -->
+  <!-- TradingView Widget END -->
+
+
+
+{slug} , {map[slug.toUpperCase()]} ,${$session.prices[slug.toUpperCase()] } , 
+
+{#each trades as trade}
+    {JSON.stringify(trade.caption)}
+
+{/each}
