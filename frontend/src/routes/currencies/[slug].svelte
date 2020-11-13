6660e75ae17cd6b8acbf3bf5d44e5d310c955dff
Isabel Friedman <isf@duke.edu>
2020-11-13 11:11:00 -0500

    fourth

--- frontend/src/routes/currencies/[slug].svelte
+++ frontend/src/routes/currencies/[slug].svelte
@@ -1,11 +1,41 @@
 <script>
+    import firebase from 'firebase/app'
+    import 'firebase/firestore'
     import { stores } from "@sapper/app";
     import { onMount } from "svelte";
+    import Trade from '../../components/Trade.svelte';
+
     const { page,session} = stores()
+
+    const map = {
+       'BTC':'Bitcoin',
+       'XRP':'Ripple',
+       'LTC':'Litecoin',
+       'DOGE':'Dogecoin',
+       'ETH':'Ethereum',
+       'LINK':'Chainlink',
+       'BCH':'Bitcoin Cash',
+       'BNB':'Binance Coin'       
+   }
+
+    let trades = []
+
+    async function getTrades() {
+   // console.log(user.uid)
+   trades = []
+    await firebase.firestore().collection("transaction").where("stock", "==", slug.toUpperCase()).limit(5)
+        .get()
+        .then(snap=>{
+            snap.forEach(doc=>{
+                trades = [...trades,doc]
+             console.log(doc.data())
+            })
+        })
+  }
 
     const { slug } = $page.params;
     onMount(()=>{
-
+        getTrades()
         new TradingView.widget(
     {
     "container_id": "technical-analysis",
@@ -34,10 +64,16 @@
     })
 </script>
 
-{slug}
+{slug.toUpperCase()} , {map[slug.toUpperCase()]} ,${$session.prices[slug.toUpperCase()] }
+
+
 <!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
     <div id="technical-analysis"></div>
     <div class="tradingview-widget-copyright">by TradingView</div>
   </div>
   <!-- TradingView Widget END -->
+
+  {#each trades as trade, index}
+  <Trade tradefull={trade} {index}/>
+{/each}
